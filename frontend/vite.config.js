560901112fa7af1cf78e078f7c8d2fe86e58882a
Ckistian-dev <crisgkist@gmail.com>
2025-05-03 13:31:19 -0300

    Atualização nova

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/', // 👈 Isso garante que todas as rotas partam da raiz
   plugins: [react()],
 })
