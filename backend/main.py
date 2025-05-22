e9293e7a13ddc2b9d54133ee72f524a39dea72e7
Ckistian-dev <crisgkist@gmail.com>
2025-05-22 14:08:21 -0300

    Atualização nova

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://www.ruachdelivery.com.br"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
